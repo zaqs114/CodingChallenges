312fb76d2cbe3e45d1489fe2721b320d798cfbe5
zaqs114 <zaqs@10g.pl>
2017-07-29 00:19:09 +0200

    heads or tails core

--- 01 - HigherLower and HeadsOrTails/src/HeadsOrTails.java
+++ 01 - HigherLower and HeadsOrTails/src/HeadsOrTails.java
@@ -23,11 +23,32 @@ public class HeadsOrTails {
             case "yes":
                     String[] options = {"heads", "tails"};
                     Random randomizer = new Random();
+
+                    int position = randomizer.nextInt(options.length);
+                    String winningside = options[position];
+
+                    System.out.println("Heads or Tails?");
+
+                    while (true){
+                        choice = read.nextLine();
+                        if(choice.equalsIgnoreCase("heads")||choice.equalsIgnoreCase("tails")){
+                            choice = choice.toLowerCase();
+                            break;
+                        }else {
+                            System.out.println("You chose wrong. Try again.");
+                        }
+                    }
+                    if (choice.equals(winningside)){
+                        System.out.println("Congratulations, you won!");
+                        HeadsOrTails headsOrTails = new HeadsOrTails();
+                    }else {
+                        System.out.println("Sadly, you lost.");
+                        HeadsOrTails headsOrTails = new HeadsOrTails();
+                    }
                 break;
             case "no":
                 Main main = new Main();
                 break;
         }
-
     }
 }
