6d2f9917e1a1d75906e59f1c3d859eab5df58c3a
zaqs114 <zaqs@10g.pl>
2017-07-28 20:53:24 +0200

    beginning of new challenge

---
+++ 01 - HigherLower and HeadsOrTails/README.md
@@ -0,0 +1,13 @@
+# 01 Higher/Lower and Heads or Tails
+
+Two simple games.
+
+
+
+### Instructions
+Clone it and compile in IntelliJ IDEA.
+
+
+   ```
+### TODO
+  * Tests
---
+++ 01 - HigherLower and HeadsOrTails/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * Created by Hubert on 28.07.2017.
+ */
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("What do you want to play?");
+        System.out.println("1 - Higher/Lower");
+        System.out.println("2 - Heads or Tails");
+        System.out.println("3 - Exit");
+
+        Scanner read = new Scanner(System.in);
+        String choice = read.nextLine();
+        if (choice == "1"){
+
+        }else if(choice == "2"){
+
+        }else if (choice == "3"){
+            System.exit(0);
+        }else{
+
+        }
+
+
+    }
+}
